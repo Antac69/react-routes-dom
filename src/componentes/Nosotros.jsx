3541ddbbfe2ddd5beca174561164fdba8f3f48d7
Antac69 <angello_04_09@hotmail.com>
2023-05-30 19:51:55 -0500

    usarnado use effect

--- src/componentes/Nosotros.jsx
+++ src/componentes/Nosotros.jsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Nosotros = () => {
+  const datos =[
+    {id:1,nombre:'Reactjs'},
+    {id:2,nombre:'Vuejs'},
+    {id:3,nombre:'Angularjs'}
+  ]
+
+  const [equipo,setEquipo] = useState(null)
+  useEffect(()=>{
+    console.log('useEffect')
+    setEquipo(datos)
+  },[])
   return (
     <div>Nosotros</div>
   )
